9c5e67b0f83c8ad6beb913db094c48c5e7984ccf
salim <boughanmi1salim@gmail.com>
2023-12-27 01:54:25 +0100

    add models service

--- news_app_with_flutter/lib/services/news_service.dart
+++ news_app_with_flutter/lib/services/news_service.dart
@@ -1,16 +1,28 @@
 import 'package:dio/dio.dart';
+import 'package:news_app_with_flutter/models/article_model.dart';
 
 class NewsService {
   final Dio dio;
   NewsService(this.dio);
   Future getNews() async {
     final Response response = await dio.get(
-        'https://newsapi.org/v2/top-headlines?country=fr&apiKey=2c549ab7bb8b412a8d49fe00a602a402&category=sports');
+        'https://newsapi.org/v2/top-headlines?country=us&apiKey=2c549ab7bb8b412a8d49fe00a602a402&category=sports');
 
     Map<String, dynamic> jsonData = response.data;
-    //print(jsonData);
 
-//    Map<String, int> ArticleData = jsonData['totalResults'];
-    print(jsonData['totalResults']);
+    List<dynamic> Articles = jsonData['articles']; // acces l'article
+
+    List<ArticleModel> articaList = []; // cree new list of articalmodel
+
+    for (var art in Articles) {
+      ArticleModel articlModel = ArticleModel(
+          //make map object in arttical model
+          image: art['urlToImage'],
+          title: art['title'],
+          subTitle: art['description']);
+
+      articaList.add(articlModel);
+    }
+    print(articaList);
   }
 }
