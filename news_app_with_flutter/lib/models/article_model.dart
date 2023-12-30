995a4b2b1e62b543110f1406a2423b7b53b3b1b6
salim <boughanmi1salim@gmail.com>
2023-12-30 20:13:02 +0100

    ajouter exeple factory constractor

--- news_app_with_flutter/lib/models/article_model.dart
+++ news_app_with_flutter/lib/models/article_model.dart
@@ -1,6 +1,6 @@
 class ArticleModel {
   final String? image;
-  final String title;
+  final String? title;
   final String? subTitle;
 
   ArticleModel({
@@ -8,4 +8,12 @@ class ArticleModel {
     required this.title,
     required this.subTitle,
   });
+  // exemple avec factoy constractor
+  factory ArticleModel.fromJson(json) {
+    return ArticleModel(
+      image: json["urlToImage"],
+      title: json["title"],
+      subTitle: json["description"],
+    );
+  }
 }
